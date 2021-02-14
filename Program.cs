6304465a26de6057793c0421e28c81a83e0bf8f0
GerasimS <58529859+GerasimS@users.noreply.github.com>
2021-02-15 00:43:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string of characters");
+            string str1 = Console.ReadLine();
+            for(int i = str1.Length - 1; i >= 0; i--)
+            {
+                Console.Write(str1[i]);
+            }
+            
+        }
+    }
+}
