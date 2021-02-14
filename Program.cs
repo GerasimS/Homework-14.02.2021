d1191d4000819000d0d5d8b60fcb5611a3e87302
GerasimS <58529859+GerasimS@users.noreply.github.com>
2021-02-15 00:48:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameR = @"[A-Z]\w*\s[A-Z]\w*";
+            string emailR = @"[a-zA-Z0-9.-]+@[a-zA-Z-]+\.(com|edu|bg|net|org)";
+            string IPv4R = @"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}";
+        }
+    }
+}
