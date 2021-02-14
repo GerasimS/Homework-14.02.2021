b09662b117059c7f535d20ae37299c0982d9d985
GerasimS <58529859+GerasimS@users.noreply.github.com>
2021-02-15 00:47:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Replace_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the word you want to remove");
+            string bword = Console.ReadLine();
+            if(bword.Contains(' '))
+            {
+                bword = bword.Remove(bword.LastIndexOf(' '));
+            }
+            Console.WriteLine("Enter text");
+            string text = Console.ReadLine();
+            string stCount = "*";
+            for (int i = bword.Length - 1; i > 0; i--)
+            {
+                stCount = stCount + "*";
+            }
+            Console.WriteLine(Regex.Replace(text, bword, stCount));
+        }
+    }
+}
